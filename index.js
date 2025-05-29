ab8d6dad1a26b0c02b3ca6a27e7c7c01d94d1633
uisl <liuw15935@gmail.com>
2025-05-29 17:17:45 +0800

    bugfix

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ if (!NOTION_SPACE_ID) {
 
 // 创建Express应用
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // 日志中间件 - 记录API调用信息
 app.use((req, res, next) => {
